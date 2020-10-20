95ff38e783227cdc0335c4177b62a2d601dfd075
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-19 19:27:14 -0700

    updated activity 29

--- Week-4/activity-29/Unsolved/script.js
+++ Week-4/activity-29/Unsolved/script.js
@@ -12,8 +12,70 @@ var totalSeconds = 0;
 var secondsElapsed = 0;
 var interval;
 
-function startTimer() {
-  // Write code to start the timer here
+
+// create init function
+//get minute value
+//convert minute value to seconds
+//store in global totalSeconds
+//clear intervals
+
+function init()
+{
+  totalSeconds = workMinutesInput.value * 60;
+  secondsElapsed = 0;
+  converTimeLeft(totalSeconds);
+  console.log(totalSeconds);
+  clearInterval(interval);
+  
+
+}
+
+
+  function converTimeLeft(seconds)
+  {
+    var minutes = Math.floor(seconds/60);
+    var sec = seconds % 60;
+    if (minutes < 10) 
+    {
+      minutes = "0"+minutes
+    }
+    if (sec < 10) 
+    {
+      sec = "0"+sec
+    }
+    minutesDisplay.textContent = minutes
+    secondsDisplay.textContent = sec
+  }
+
+function startTimer() 
+{
+  interval = setInterval(function()
+    {
+      if (secondsElapsed <= totalSeconds){
+      secondsElapsed++;
+      var secondsLeft =totalSeconds - secondsElapsed;
+      converTimeLeft(secondsLeft)
+    }
+    else 
+    {
+      clearInterval(interval);
+      alert('Time to take a break');
+    } 
+  }, 1000)
+};
+
+function pauseTimer()
+{
+  clearInterval(interval);
+}
+
+function resetTimer()
+{
+  init()
 }
 
 playButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
+stopButton.addEventListener("click", resetTimer);
+
+init()
